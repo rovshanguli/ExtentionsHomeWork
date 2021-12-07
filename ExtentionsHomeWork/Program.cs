8a3b2f1dc58dbbf302a70242b9fcf396daa0fa54
rovshanguli <rovshanshg@code.edu.az>
2021-12-08 01:58:31 +0400

    Extetions

---
+++ ExtentionsHomeWork/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ExtentionsHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "Salam Cavid.Necesen Cavid";
+            int count = sentence.SumOfWord("Cavid");
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ ExtentionsHomeWork/StringExtention.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtentionsHomeWork
+{
+    public static class StringExtention
+    {
+
+        public static int SumOfWord(this string sentence, string word)
+        {
+            int sumofword = 0;
+            for(int i = 0; i < sentence.Length; i++)
+            {
+                if (sentence.Contains(word))
+                {
+                    sumofword++;
+                }
+            }
+            return sumofword;
+        }
+    }
+}
